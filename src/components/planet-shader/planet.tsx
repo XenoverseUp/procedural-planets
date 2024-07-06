4bc2a04a99563eaa160b0ab4c74b293ff24a9897
XenoverseUp <xenoverseup@gmail.com>
2024-07-06 22:13:53 +0300

    feat: shader based implementation

--- src/components/planet-shader/planet.tsx
+++ src/components/planet-shader/planet.tsx
@@ -1,4 +1,3 @@
-import TerrainFace from "./terrain-face";
 import {
   VECTOR_BACK,
   VECTOR_DOWN,
@@ -11,51 +10,65 @@ import { useAtomValue } from "jotai";
 import {
   isWireframeAtom,
   meshResolutionAtom,
+  noiseFiltersAtom,
   planetRadiusAtom,
   rendersGlobeAtom,
 } from "@/atoms/settings";
 
-import WireFace from "./wire-face";
-
-const directions = [
-  VECTOR_UP,
-  VECTOR_DOWN,
-  VECTOR_LEFT,
-  VECTOR_RIGHT,
-  VECTOR_FRONT,
-  VECTOR_BACK,
-];
+import vs from "@/glsl/terrain.vs?raw";
+import fs from "@/glsl/terrain.fs?raw";
+import { useLayoutEffect, useRef } from "react";
+import { GLSL3, ShaderMaterial } from "three";
+import { SimpleNoiseFilter } from "@/lib/noise";
 
 const Planet = () => {
   const meshResolution = useAtomValue(meshResolutionAtom);
   const isWireframe = useAtomValue(isWireframeAtom);
   const planetRadius = useAtomValue(planetRadiusAtom);
   const rendersGlobe = useAtomValue(rendersGlobeAtom);
+  const noiseFilters = useAtomValue(noiseFiltersAtom);
+
+  const shaderRef = useRef<ShaderMaterial>(null);
+
+  useLayoutEffect(() => {
+    if (!shaderRef.current) return;
+    shaderRef.current.uniforms.uRadius = {
+      value: planetRadius,
+    };
+
+    shaderRef.current.uniforms.uResolution = {
+      value: meshResolution,
+    };
+
+    shaderRef.current.uniforms.uStrength = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).strength,
+    };
+
+    shaderRef.current.uniforms.uBaseRoughness = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).baseRoughness,
+    };
+
+    shaderRef.current.uniforms.uLayerCount = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).layerCount,
+    };
+
+    shaderRef.current.uniforms.uRoughness = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).roughness,
+    };
+
+    shaderRef.current.uniforms.uPersistence = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).persistence,
+    };
+
+    shaderRef.current.uniforms.uMinValue = {
+      value: (noiseFilters.at(0) as SimpleNoiseFilter).minValue,
+    };
+  }, [planetRadius, noiseFilters]);
 
   return (
     <mesh>
-      {directions.map((direction, i) => {
-        if (rendersGlobe || direction === VECTOR_FRONT)
-          return (
-            <TerrainFace
-              wireframe={isWireframe}
-              resolution={meshResolution}
-              radius={planetRadius}
-              localUp={direction}
-              key={`terrain-face-${i}`}
-              renderBackface={!rendersGlobe}
-            />
-          );
-
-        return (
-          <WireFace
-            resolution={10}
-            radius={planetRadius}
-            localUp={direction}
-            key={`wire-face-${i}`}
-          />
-        );
-      })}
+      <icosahedronGeometry args={[1, meshResolution / 2]} />
+      <shaderMaterial ref={shaderRef} vertexShader={vs} fragmentShader={fs} />
     </mesh>
   );
 };
--- src/glsl/terrain.fs
+++ src/glsl/terrain.fs
@@ -3,7 +3,9 @@ precision mediump float;
 uniform float uRadius;
 
 varying vec3 vNormal;
+varying float vElevation;
 
 void main() {
-    gl_FragColor = vec4(vNormal, 1);
+    vec3 color = vec3(vNormal);
+    gl_FragColor = vec4(color, 1);
 }
