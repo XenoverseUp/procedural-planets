ea5f93db3f322ee10901cd4965cbf2e39d1dfbda
XenoverseUp <xenoverseup@gmail.com>
2024-07-21 01:47:09 +0300

    feat: showcase

--- src/components/composed/sidebar.tsx
+++ src/components/composed/sidebar.tsx
@@ -13,6 +13,7 @@ import { SimpleNoiseFilter } from "@/lib/noise";
 import { VECTOR_ZERO } from "@/lib/vector";
 import { MixerHorizontalIcon, PaperPlaneIcon } from "@radix-ui/react-icons";
 import { useAtom } from "jotai";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
   const [meshResolution, setMeshResolution] = useAtom(meshResolutionAtom);
@@ -22,87 +23,95 @@ const Sidebar = () => {
   const depthGradientState = useAtom(depthGradientAtom);
 
   return (
-    <aside className="flex h-full w-96 flex-shrink-0 flex-col overflow-hidden rounded-lg bg-white">
-      <header className="border-b px-4 py-3">
-        <h2 className="flex items-center gap-2 text-sm font-medium">
-          <MixerHorizontalIcon />
-          <span>Control Panel</span>
-        </h2>
-      </header>
-      <div className="space-y-8 overflow-y-auto overflow-x-hidden p-4 pb-8">
-        <SliderInput
-          label="Planet Radius"
-          defaultValue={planetRadius}
-          onValueChange={setPlanetRadius}
-          labels={["1", "2"]}
-          min={1}
-          max={2}
-          step={0.05}
-        />
-        <SliderInput
-          label="Resolution"
-          description="Defines the level resolution of the planet mesh. The higher this value, the more room for detail. The lower this value, the higher performance."
-          defaultValue={meshResolution}
-          onValueChange={setMeshResolution}
-          labels={["8", "512"]}
-          min={8}
-          max={512}
-          step={2}
-        />
-        <section>
-          <StepInput
-            label="Noise Settings"
-            description="Defines the level of detail (LOD) for the generated terrain. Max number of layers is 3."
-            defaultValue={noiseFilters.length}
-            onIncrement={() => {
-              setNoiseFilters((filters) => [
-                ...filters,
-                new SimpleNoiseFilter({
-                  enabled: true,
-                  strength: 0.2,
-                  roughness: 2.25,
-                  baseRoughness: 0.7,
-                  center: VECTOR_ZERO,
-                  persistence: 0.5,
-                  minValue: 1.1,
-                  layerCount: 10,
-                  useFirstLayerAsMask: false,
-                }),
-              ]);
-            }}
-            onDecrement={() => {
-              setNoiseFilters((filters) => [
-                ...filters.slice(0, filters.length - 1),
-              ]);
-            }}
+    <motion.div
+      exit={{
+        width: 0,
+        opacity: 0,
+      }}
+      className="overflow-hidden"
+    >
+      <aside className="mr-2 flex h-full w-96 flex-shrink-0 flex-col overflow-hidden rounded-lg bg-white">
+        <header className="border-b px-4 py-3">
+          <h2 className="flex items-center gap-2 text-sm font-medium">
+            <MixerHorizontalIcon />
+            <span>Control Panel</span>
+          </h2>
+        </header>
+        <div className="space-y-8 overflow-y-auto overflow-x-hidden p-4 pb-8">
+          <SliderInput
+            label="Planet Radius"
+            defaultValue={planetRadius}
+            onValueChange={setPlanetRadius}
+            labels={["1", "2"]}
+            min={1}
+            max={2}
+            step={0.05}
+          />
+          <SliderInput
+            label="Resolution"
+            description="Defines the level resolution of the planet mesh. The higher this value, the more room for detail. The lower this value, the higher performance."
+            defaultValue={meshResolution}
+            onValueChange={setMeshResolution}
+            labels={["8", "512"]}
+            min={8}
+            max={512}
+            step={2}
+          />
+          <section>
+            <StepInput
+              label="Noise Settings"
+              description="Defines the level of detail (LOD) for the generated terrain. Max number of layers is 3."
+              defaultValue={noiseFilters.length}
+              onIncrement={() => {
+                setNoiseFilters((filters) => [
+                  ...filters,
+                  new SimpleNoiseFilter({
+                    enabled: true,
+                    strength: 0.2,
+                    roughness: 2.25,
+                    baseRoughness: 0.7,
+                    center: VECTOR_ZERO,
+                    persistence: 0.5,
+                    minValue: 1.1,
+                    layerCount: 10,
+                    useFirstLayerAsMask: false,
+                  }),
+                ]);
+              }}
+              onDecrement={() => {
+                setNoiseFilters((filters) => [
+                  ...filters.slice(0, filters.length - 1),
+                ]);
+              }}
+            />
+
+            <div className="mt-4 w-full space-y-2">
+              {noiseFilters.map((_, i) => (
+                <NoiseSettingsDetail
+                  key={`noise-filter-${i}`}
+                  index={i}
+                  title={`Noise Setting ${i + 1}`}
+                />
+              ))}
+            </div>
+          </section>
+
+          <GradientInput
+            label="Elevation"
+            description="Assigns different colors to different elevations on the planet. 0,0 means the lowest vertex and 1,0 means the highest vertex."
+            datasetKey="elevationThumb"
+            gradientState={elevationGradientState}
           />
 
-          <div className="mt-4 w-full space-y-2">
-            {noiseFilters.map((_, i) => (
-              <NoiseSettingsDetail
-                key={`noise-filter-${i}`}
-                index={i}
-                title={`Noise Setting ${i + 1}`}
-              />
-            ))}
-          </div>
-        </section>
-
-        <GradientInput
-          label="Elevation"
-          description="Assigns different colors to different elevations on the planet. 0,0 means the lowest vertex and 1,0 means the highest vertex."
-          datasetKey="elevationThumb"
-          gradientState={elevationGradientState}
-        />
-
-        <GradientInput
-          label="Depth"
-          description="Assigns different colors to different depths in the sea. 0,0 means the lowest vertex and 1,0 means the highest vertex in the sea."
-          datasetKey="depthThumb"
-          gradientState={depthGradientState}
-        />
-      </div>
-    </aside>
+          <GradientInput
+            label="Depth"
+            description="Assigns different colors to different depths in the sea. 0,0 means the lowest vertex and 1,0 means the highest vertex in the sea."
+            datasetKey="depthThumb"
+            gradientState={depthGradientState}
+          />
+        </div>
+      </aside>
+    </motion.div>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,36 +7,43 @@ import { PaperPlaneIcon } from "@radix-ui/react-icons";
 import { Float, OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const [isShowcase, setIsShowcase] = useState(false);
+
   return (
-    <main className="flex h-screen w-full select-none gap-2 bg-blue-200 p-2">
-      <div className="dotted relative h-full flex-grow items-center overflow-hidden rounded-lg bg-blue-50">
-        <Toolbar className="absolute bottom-2 left-2 z-10" />
-        <Canvas>
-          <Lights />
-          <OrbitControls enableZoom={true} enablePan={false} />
-          <Stats />
-          {/* <Float
+    <main className="flex h-screen w-full select-none bg-blue-200 p-2 pr-0">
+      <div className="dotted relative mr-2 h-full flex-grow rounded-lg bg-blue-50">
+        <div className="mx-auto h-full w-[calc(100vw_-_26rem)] overflow-hidden">
+          <Toolbar className="absolute bottom-2 left-2 z-10" />
+          <Canvas className="w-full">
+            <Lights />
+            <OrbitControls enableZoom={false} enablePan={false} />
+            <Stats />
+            {/* <Float
             speed={3}
             rotationIntensity={0.5}
             floatIntensity={1}
             floatingRange={[-0.02, 0.02]}
           >
           </Float> */}
-          <PlanetGPU />
-          {/* <Planet /> */}
-        </Canvas>
+            <PlanetGPU />
+            {/* <Planet /> */}
+          </Canvas>
 
-        <Link
-          to="/showcase"
-          className="absolute bottom-4 right-4 flex items-center gap-1 rounded-full bg-gradient-to-br from-blue-200 to-blue-300 px-4 py-1 text-sm text-blue-900 hover:brightness-95"
-        >
-          <span>Finish</span>
-          <PaperPlaneIcon />
-        </Link>
+          <button
+            onClick={() => setIsShowcase((state) => !state)}
+            className="absolute bottom-4 right-4 flex items-center gap-1 rounded-full bg-gradient-to-br from-blue-200 to-blue-300 px-4 py-1 text-sm text-blue-900 hover:brightness-95"
+          >
+            <span>Finish</span>
+            <PaperPlaneIcon />
+          </button>
+        </div>
       </div>
-      <Sidebar />
+
+      <AnimatePresence>{!isShowcase && <Sidebar />}</AnimatePresence>
     </main>
   );
 }
