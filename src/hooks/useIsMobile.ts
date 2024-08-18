1651532017467646e2a39973b0e7b48c12bdc8a2
XenoverseUp <xenoverseup@gmail.com>
2024-08-18 18:05:57 +0300

    fix: ts errors

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -4,12 +4,11 @@ function useIsMobile() {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const userAgent = navigator.userAgent || navigator?.vendor || window?.opera;
+    const userAgent = navigator.userAgent || navigator?.vendor;
 
     // Check for mobile user agents
     if (/android/i.test(userAgent)) setIsMobile(true);
-    else if (/iPhone|iPad|iPod/i.test(userAgent) && !window?.MSStream)
-      setIsMobile(true);
+    else if (/iPhone|iPad|iPod/i.test(userAgent)) setIsMobile(true);
     else setIsMobile(false);
   }, []);
 
